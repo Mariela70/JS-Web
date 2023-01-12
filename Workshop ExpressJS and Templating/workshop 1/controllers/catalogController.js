576dfd2106d9adc00804f8e05d071a523367ec56
Mariela <marielamiteva97@abv.bg>
2023-01-12 18:03:26 +0200

    Implemented Catalog and Details Implemented search functionality

--- Workshop ExpressJS and Templating/workshop 1/controllers/catalogController.js
+++ Workshop ExpressJS and Templating/workshop 1/controllers/catalogController.js
@@ -1,14 +1,38 @@
+const { getAll, getbyId } = require('../services/accomodationService');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
+    const search = req.query.search || '';
+    const city = req.query.city || '';
+    const fromPrice = Number(req.query.fromPrice || 1);
+    const toPrice = Number(req.query.toPrice || 1000);
+    const rooms = getAll(search, city, fromPrice, toPrice)
     res.render('catalog', {
-        title: 'All Accomodation'
+        title: 'All Accomodation',
+        rooms,
+        search,
+        city,
+        fromPrice,
+        toPrice
     });
 });
 router.get('/:id', (req, res) => {
-    res.render('details', {
-        title: 'Accomodation Details'
-    });
+    const roomId = req.params.id;
+    const room = getbyId(roomId);
+    if(room) {
+        res.render('details', {
+            title: 'Accomodation Details',
+            room
+        });
+
+    }else {
+        res.render('roomNotFound', {
+            title: 'Accomodation Details',
+            roomId
+        });
+    }
+    
 });
 
 module.exports = router;
