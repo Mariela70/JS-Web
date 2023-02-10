eea7da0e3cb2c4160ed23e0358da3781e20b210e
Mariela <marielamiteva97@abv.bg>
2023-02-10 02:33:36 +0200

    added page 404

--- Exam Preparation/Wildlife Photography/config/routes.js
+++ Exam Preparation/Wildlife Photography/config/routes.js
@@ -8,4 +8,9 @@ app.use('/', homeController);
 app.use('/auth', authController);
 app.use(postController);
 
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Page Not Found'
+    });
+})
 };
