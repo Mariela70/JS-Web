b7dbd106a40aa14b9b1596e656a57d59c32f734a
Mariela <marielamiteva97@abv.bg>
2023-02-12 23:51:21 +0200

    added isOwner

--- Exam Preparation/Shared Trip/middlewares/guards.js
+++ Exam Preparation/Shared Trip/middlewares/guards.js
@@ -1,22 +1,33 @@
 function hasUser() {
     return (req, res, next) => {
-        if(req.user) {
+        if (req.session.user) {
             next();
-        }else {
+        } else {
             res.redirect('/auth/login');
         }
     };
 }
 function isGuest() {
     return (req, res, next) => {
-        if(req.user) {
+        if (req.session.user) {
             res.redirect('/');
-        }else {
+        } else {
             next();
         }
     };
 }
+function isOwner() {
+    return function (req, res, next) {
+        const userId = req.session.user?._id;
+        if (res.locals.trip.owner == userId) {
+            next();
+        } else {
+            res.redirect('/auth/login');
+        }
+    }
+}
 module.exports = {
     hasUser,
-    isGuest
+    isGuest,
+    isOwner
 };
