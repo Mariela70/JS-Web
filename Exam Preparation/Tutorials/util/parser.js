f72aa76bb3b318b08df1f80f8a59a215cc14a645
Mariela <marielamiteva97@abv.bg>
2023-02-15 16:48:48 +0200

    util

--- Exam Preparation/Tutorials/util/parser.js
+++ Exam Preparation/Tutorials/util/parser.js
@@ -1,11 +1,10 @@
 function parseError(error) {
-    if (error.name = 'ValidationError') {
+    if (error.name == 'ValidationError') {
         return Object.values(error.errors).map(v => v.message);
 
     } else if (Array.isArray(error)) {
         return error.map(x => x.msg);
-    }
-    else {
+    } else {
         return error.message.split('\n');
 
     }
