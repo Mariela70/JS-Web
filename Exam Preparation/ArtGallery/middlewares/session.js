12d71de3e16f55cdd7b46eca023962bf88f70e87
Mariela <marielamiteva97@abv.bg>
2023-02-16 23:52:29 +0200

    middlewares

---
+++ Exam Preparation/ArtGallery/middlewares/session.js
@@ -0,0 +1,19 @@
+const { verifyToken } = require("../services/userService");
+
+module.exports = () => (req, res, next) => {
+    const token = req.cookies.token;
+    if(token) {
+        try {
+            const userData = verifyToken(token);
+            req.user = userData;
+            req.user.username = userData.username;
+
+        } catch(err) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return;
+
+        }
+    }
+    next();
+}
